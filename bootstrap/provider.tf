d2fd117b39b9c69169fe4c4c301093ca0d0e7e27
Pawel-Barej <barej8pawel@gmail.com>
2023-11-29 21:54:04 +0100

    changed authorization

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -1,11 +1,13 @@
 provider "google" {
   project = local.project
   region  = var.region
+  #  credentials = file("credentials.json")
 }
 terraform {
   required_version = "~> 1.5.0"
   required_providers {
     google = {
+      source  = "hashicorp/google"
       version = "~> 4.84.0"
     }
   }
--- bootstrap/variables.tf
+++ bootstrap/variables.tf
@@ -1,18 +1,21 @@
 variable "tbd_semester" {
   type        = string
   description = "TBD semester"
+  default     = "2023Z"
 }
 
 variable "user_id" {
   type        = number
   description = "TBD project group id"
+  default     = "3004780"
 }
 variable "billing_account" {
   type        = string
   description = "Billing account a project is attached to"
+  default     = "012FE3-08A5B5-978423"
 }
 variable "region" {
   type        = string
-  default     = "europe-west1"
   description = "GCP region"
+  default     = "europe-west1"
 }
