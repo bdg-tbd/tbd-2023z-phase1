cc5d9d974e2807950f842d445856840df3125860
jakubpludowski <pludy99@gmail.com>
2023-12-01 20:06:06 +0000

    task 15.3

--- modules/vertex-ai-workbench/main.tf
+++ modules/vertex-ai-workbench/main.tf
@@ -56,6 +56,9 @@ resource "google_notebooks_instance" "tbd_notebook" {
     repository = var.ai_notebook_image_repository
     tag        = var.ai_notebook_image_tag
   }
+  shielded_instance_config {
+    enable_secure_boot = true
+  }
   network = var.network
   subnet  = var.subnet
   ## change it to break the checkov during the labs
