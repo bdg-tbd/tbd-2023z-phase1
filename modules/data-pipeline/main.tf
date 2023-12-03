f5a3c9657b685b805e02a6e00cea718d07e46f46
Pawel-Barej <barej8pawel@gmail.com>
2023-12-03 21:43:33 +0100

    test

--- modules/data-pipeline/main.tf
+++ modules/data-pipeline/main.tf
@@ -6,21 +6,21 @@ locals {
 }
 
 
-# resource "google_storage_bucket" "tbd-code-bucket" {
-#   project                     = var.project_name
-#   name                        = var.bucket_name
-#   location                    = var.region
-#   uniform_bucket_level_access = false #tfsec:ignore:google-storage-enable-ubla
-#   force_destroy               = true
-#   versioning {
-#     enabled = true
-#   }
+resource "google_storage_bucket" "tbd-code-bucket" {
+  project                     = var.project_name
+  name                        = var.bucket_name
+  location                    = var.region
+  uniform_bucket_level_access = false #tfsec:ignore:google-storage-enable-ubla
+  force_destroy               = true
+  versioning {
+    enabled = true
+  }
 
-#   #checkov:skip=CKV_GCP_62: "Bucket should log access"
-#   #checkov:skip=CKV_GCP_29: "Ensure that Cloud Storage buckets have uniform bucket-level access enabled"
-#   #checkov:skip=CKV_GCP_78: "Ensure Cloud storage has versioning enabled"
-#   public_access_prevention = "enforced"
-# }
+  #checkov:skip=CKV_GCP_62: "Bucket should log access"
+  #checkov:skip=CKV_GCP_29: "Ensure that Cloud Storage buckets have uniform bucket-level access enabled"
+  #checkov:skip=CKV_GCP_78: "Ensure Cloud storage has versioning enabled"
+  public_access_prevention = "enforced"
+}
 
 resource "google_storage_bucket_iam_member" "tbd-code-bucket-iam-viewer" {
   bucket = google_storage_bucket.tbd-code-bucket.name
@@ -43,18 +43,18 @@ resource "google_storage_bucket_object" "dag-code" {
   source   = "${path.module}/resources/${each.value}"
 }
 
-resource "google_storage_bucket" "tbd-data-bucket" {
-  project                     = var.project_name
-  name                        = var.bucket_name
-  location                    = var.region
-  uniform_bucket_level_access = false #tfsec:ignore:google-storage-enable-ubla
-  force_destroy               = true
-  public_access_prevention    = "enforced"
-  # Add other properties if needed to handle errors from checkcov
-}
+# resource "google_storage_bucket" "tbd-data-bucket" {
+#   project                     = var.project_name
+#   name                        = var.bucket_name
+#   location                    = var.region
+#   uniform_bucket_level_access = false #tfsec:ignore:google-storage-enable-ubla
+#   force_destroy               = true
+#   public_access_prevention    = "enforced"
+#   # Add other properties if needed to handle errors from checkcov
+# }
 
-resource "google_storage_bucket_iam_member" "tbd-data-bucket-iam-editor" {
-  bucket = google_storage_bucket.tbd-data-bucket.name
-  role   = "roles/storage.objectUser"
-  member = "serviceAccount:${var.data_service_account}"
-}
+# resource "google_storage_bucket_iam_member" "tbd-data-bucket-iam-editor" {
+#   bucket = google_storage_bucket.tbd-data-bucket.name
+#   role   = "roles/storage.objectUser"
+#   member = "serviceAccount:${var.data_service_account}"
+# }
